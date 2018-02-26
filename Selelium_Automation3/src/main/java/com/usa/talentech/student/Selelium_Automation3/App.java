c898b1c8130a8f5b806ea42debd63f521a753d40
USER <USER@Lenovo-PC>
2018-02-25 20:41:18 -0500

    commit by ahad

---
+++ Selelium_Automation3/src/main/java/com/usa/talentech/student/Selelium_Automation3/App.java
@@ -0,0 +1,13 @@
+package com.usa.talentech.student.Selelium_Automation3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Selelium_Automation3/src/main/java/com/usa/talentech/student/Selelium_Automation3/Firefox_Driver3.java
@@ -0,0 +1,18 @@
+package com.usa.talentech.student.Selelium_Automation3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firefox_Driver3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebbok.com");
+		
+		
+		
+	}
+
+}
